4bc73d36cad8ee1268265a0dd14f2ea0f23d9553
schmeddy99 <ahmedaosman00@gmail.com>
2025-06-02 10:24:00 -0700

    Updating for loop so that previous guesses are added to display, adding a while loop to continue game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -12,16 +11,24 @@ for index in range(word_length):
     placeholder += "_"
 print(placeholder)
 
+game_over = False
+correct_letters = []
 
-guess = input("Guess a letter: ").lower()
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
+    display = ""
+    for char in chosen_word:
+        if guess == char:
+            display += char
+            correct_letters.append(guess)
+        elif char in correct_letters:
+            display += char
+        else:
+            display += "_"
 
-display = ""
-for char in chosen_word:
-    if guess == char:
-        display += char
-    else:
-        display += "_"
+    print(display)
 
-
-print(display)
+    if "_" not in display:
+        game_over = True
+        print("Congrats! You win!")
