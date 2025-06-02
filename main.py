04954d6e7399e53fa02637d7e74460b4e6c7c676
schmeddy99 <ahmedaosman00@gmail.com>
2025-06-02 10:26:35 -0700

    Adding lives variable to keep track of lives left. if guess is not in the chosen word, subtract life and player loses

--- main.py
+++ main.py
@@ -1,6 +1,72 @@
 import random
 
+stages = [
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    r"""
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
 word_list = ["aardvark", "baboon", "camel"]
+
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -28,6 +94,12 @@ while not game_over:
             display += "_"
 
     print(display)
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            game_over = True
+            print("RIP.")
 
     if "_" not in display:
         game_over = True
